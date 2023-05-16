a3a22e8b3de4afe815db915b30a1d60e5b2ad889
ZxzStarfish <1357738012@qq.com>
2023-05-16 17:07:32 +0800

    zxzNew 2023/5/16 17:07

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,11 @@ const routes = [
         path: '/OrderList',
         name: 'OrderList',
         component: () => import('@/components/OrderList')
+    },
+    {
+        path: '/HotelOrder',
+        name: 'HotelOrder',
+        component: () => import('@/components/HotelOrder')
     }
 ]
 export const router = createRouter({
