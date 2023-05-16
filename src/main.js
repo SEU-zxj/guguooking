768f51ef2506002b4d646dd8e44c3105047ba985
Student.Xiaoji <73421144+SEU-zxj@users.noreply.github.com>
2023-05-16 15:33:36 +0800

    zxj add http

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import axios from '@/plugins/axiosInstance'
+import store from '@/store/index'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
-
+app.config.globalProperties.$axios=axios;
+app.config.globalProperties.$store=store;
+app.use(store)
 app.mount('#app')
-
